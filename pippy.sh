dbc2c6a9d826ca22755a8d9c3d4899ba07587dc9
gimpdebian <94876123+gimpdebian@users.noreply.github.com>
2024-03-13 17:13:37 -0600

    automatic installation of npm packages used in custom js code and bugfixes

--- pippy.sh
+++ pippy.sh
@@ -2,11 +2,38 @@
 python_paths=$(find /usr/bin /usr/local/bin /bin ~/.local/bin ~/.local/lib ~ /opt -maxdepth 2 -type f -name "python*" 2>/dev/null)
 npm --version &>/dev/null && echo "npm is already installed, updating..." && npm install -g npm || echo "npm is not installed, installing..." && apt-get install -y npm || sudo apt-get install -y npm
 IFS=$' ' read -r -a gradio_packages <<< "$(npm search @gradio --parseable --searchlimit=2147483647 | awk '{print $1}' | tr '\n' ' ')"
+extract_js_values() {
+    echo "$1" > temp_script.py
+    js_values=$("$highest_version_path" - <<EOF
+import ast
+
+
+with open('temp_script.py', 'r') as file:
+    script_content = file.read()
+parsed_script = ast.parse(script_content)
+js_values = []
+for node in ast.walk(parsed_script):
+    if isinstance(node, ast.Assign):
+        for target in node.targets:
+            if isinstance(target, ast.Name) and target.id == 'js':
+                js_value = node.value
+                js_values.append(repr(eval(compile(ast.Expression(js_value), '<string>', 'eval'))))
+    elif isinstance(node, ast.Call):
+        for keyword in node.keywords:
+            if keyword.arg == 'js':
+                js_value = keyword.value
+                js_values.append(repr(eval(compile(ast.Expression(js_value), '<string>', 'eval'))))
+print(js_values)
+EOF
+)
+    echo "$js_values"
+    rm temp_script.py
+}
 function module_available_on_pypi() {
     local module="$1"
     local response
     response=$(curl -s "https://pypi.org/pypi/$module/json")
-    if [[ $response == *"\"$module\""* ]]; then
+    if [ $? -eq 0 ] && [[ $response == *"\"$module\""* ]]; then
         return 0
     else
         return 1
@@ -52,11 +79,26 @@ for library in $library_names; do
     fi
 done
 code=$(cat "$script_name")
+library_names_js=$(grep -oP 'import\s+\K[a-zA-Z0-9_]+' extract_js_values "$code")
 if [[ "$code" == *"import gradio as gr"* ]]; then
     echo "Installing gradio npm packages..."
     for package in "${gradio_packages[@]}"; do
-        npm install "$package"
+        npm install --global "$package"
     done
+    if [ -n "$library_names_js" ]; then
+        echo "Importing custom js npm packages..."
+        for library in $library_names; do
+            if npm show "$library" &>/dev/null; then
+                if npm outdated --json "$library" | grep -q '"current"' && return 0 || return 1; then
+                    echo "Upgrading" "$library"
+                    npm update --global "$library"
+                else
+                    echo "Installing" "$library"
+                    npm install --global "$library"
+                fi
+            fi
+        done
+    fi
 fi
 echo "Executing script..."
 "$highest_version_path" "$script_name"
